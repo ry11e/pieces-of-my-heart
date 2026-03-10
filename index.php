ec4cd633891a5f338a153c95d31cce39ae67af93
Rylle <panganonongrylle@gmail.com>
2026-03-10 15:41:22 +0800

    add popup and button

--- index.php
+++ index.php
@@ -1,11 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        
+    </style>
 </head>
+
 <body>
     Potatoes
+
+    <div>
+        <button onclick="document.getElementById('myPopup').showModal()">Click me</button>
+    </div>
+
+    <dialog id="myPopup">
+        <h2>Hello!</h2>
+        <p>This is a simple popup using only HTML + a tiny bit of JavaScript.</p>
+        <p>You can put anything here: forms, images, messages...</p>
+
+        <button class="close-btn" onclick="document.getElementById('myPopup').close()">
+            Close
+        </button>
+    </dialog>
 </body>
+
 </html>
